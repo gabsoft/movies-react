e6e5ccf20277118ffc9a749764c9aa1e09f4af89
Gabriel Almeida <gabriel@agenciaeplus.com.br>
2022-10-19 10:01:09 -0300

    moovie react test

---
+++ src/component/Search.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+
+function Search(props) {
+ 
+    const [searchValue, setSearchValue] = useState("")
+   
+    const handleSearchInputChanges = (e) => {
+        setSearchValue(e.target.value)
+    }
+
+    const resetInputFields = () => {
+        setSearchValue("")
+    }
+
+    const callSearchFunction = (e) => {
+        e.preventDefault()
+        console.log(props)
+        console.log(props.search(searchValue))
+        resetInputFields()
+    }
+
+    return (
+        <form className="search">
+            <input value={searchValue} onChange={handleSearchInputChanges} type="text"  />
+            <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+        </form>
+    )
+
+
+}
+
+
+export default Search
